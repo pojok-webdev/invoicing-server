7ea9b119c0b439fc17c561b171f6e772e40d7047
pujie <puji@gmail.com>
2018-10-24 09:00:53 +0700

    return the sql

--- js/queries.js
+++ js/queries.js
@@ -1,12 +1,14 @@
 var getclient = obj => {
         sql = 'select client_id,a.name,b.name,a.siup,a.npwp,a.npwpaddress from fbs a         ';
         sql+= 'left outer join clients b on b.id=a.client_id ';
-console.log('sql',sql)
+        console.log('sql',sql)
         return sql;
     },
     getServices = obj => {
         sql = 'select a.id,a.name,concat(c.category," ",c.bandwidth," ",upstr," ",dnstr) svc '
         sql+= 'from clients a left outer join fbs b on b.client_id=a.id left outer join fbservices c on c.fb_id=b.nofb;'
+        console.log('getservices',sql)
+        return sql
     }
     module.exports = {
         getclient:getclient,
